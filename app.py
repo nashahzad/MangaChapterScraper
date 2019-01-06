7988acbdf692ee1512c195f2b40b712ce57605c6
Nauman Shahzad <nashahzad@cs.stonybrook.edu>
2019-01-06 02:24:26 -0500

    Added an optional dark theme

--- app.py
+++ app.py
@@ -4,19 +4,23 @@ from crawler import ImageCrawler
 
 app = Flask(__name__)
 Bootstrap(app)
-
+light = True
 
 @app.route('/')
 def hello_world():
-    return render_template('query_page.html')
+    args = request.args.to_dict()
+    global light
+    light = True if args.get('light', 'true') == 'true' else False
+    return render_template('query_page.html', light=light)
 
 @app.route('/search')
 def search():
     args = request.args.to_dict()
     crawler = ImageCrawler(args)
     images = crawler.crawl()
-    return render_template('image_results.html', images=images)
+    global light
+    return render_template('image_results.html', images=images, light=light)
 
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=80, debug=True)
+    app.run("0.0.0.0", port=80)
